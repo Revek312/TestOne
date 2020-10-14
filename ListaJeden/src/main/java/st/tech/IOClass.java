1ad0d9be63a9e128a68a5e4496aa11b2ea4c1aaf
Jakub <revek312@gmail.com>
2020-10-14 14:11:52 +0200

    IOClass completed.

--- ListaJeden/src/main/java/st/tech/IOClass.java
+++ ListaJeden/src/main/java/st/tech/IOClass.java
@@ -1,5 +1,19 @@
 package st.tech;
 
+import java.util.Scanner;
+
 public class IOClass {
 
+	public void printString(String str) {
+		System.out.println(str);
+	}
+	
+	public String getString() {
+		Scanner myObj = new Scanner(System.in);
+	    System.out.println("Input line:");
+
+	    String line = myObj.nextLine();
+	    myObj.close();
+	    return line;
+	}
 }
